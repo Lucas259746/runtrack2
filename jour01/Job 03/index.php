8d9252611f96cd07b9abb70ade686bb81416e282
Lucas259746 <lucas.rozotte@laplateforme.io>
2025-08-01 12:34:26 +0200

    small change

--- jour01/Job 03/index.php
+++ jour01/Job 03/index.php
@@ -1,39 +1,43 @@
 <?php
-// Déclaration de variables
-$nom = "Jean";
-$age = 30;
-$estEtudiant = true;
-$notes = [15, 12, 17];
+$chaine_de_caractere = "lucas";
+$int = 24;
+$bool = true;
+$float = 1.5;
 
-// Stockage des variables dans un tableau associatif
-$variables = [
-    "nom" => $nom,
-    "age" => $age,
-    "estEtudiant" => $estEtudiant,
-    "notes" => $notes
-];
 
-// Début du tableau HTML
 echo "<table border='1' cellpadding='10'>";
-echo "<thead><tr><th>Type</th><th>Nom</th><th>Valeur</th></tr></thead>";
+echo "<thead>
+            <tr>
+                <th>Type</th>
+                <th>Nom</th>
+                <th>Valeur</th>
+            </tr>
+     </thead>";
+
 echo "<tbody>";
 
-// Boucle pour générer chaque ligne
-foreach ($variables as $nomVar => $valeur) {
     echo "<tr>";
-    echo "<td>" . gettype($valeur) . "</td>";
-    echo "<td>" . $nomVar . "</td>";
-    
-    // Conversion des tableaux en chaîne lisible
-    if (is_array($valeur)) {
-        echo "<td>" . implode(", ", $valeur) . "</td>";
-    } elseif (is_bool($valeur)) {
-        echo "<td>" . ($valeur ? "true" : "false") . "</td>";
-    } else {
-        echo "<td>" . $valeur . "</td>";
-    }
+        echo "<td>" . gettype($chaine_de_caractere) . "</td>";
+            echo "<td>chaine_de_caractere</td>";
+        echo "<td>" . $chaine_de_caractere . "</td>";
     echo "</tr>";
-}
+    echo "<tr>";
+        echo "<td>" . gettype($int) . "</td>";
+            echo "<td>int</td>";
+        echo "<td>" . $int . "</td>";
+    echo "</tr>";
+    echo "<tr>";
+        echo "<td>" . gettype($bool) . "</td>";
+            echo "<td>bool</td>";
+        echo "<td>" . $bool . "</td>";
+    echo "</tr>";
+    echo "<tr>";
+        echo "<td>" . gettype($float) . "</td>";
+            echo "<td>float</td>";
+        echo "<td>" . $float . "</td>";
+    echo "</tr>";
+
+
 
 echo "</tbody></table>";
 ?>
