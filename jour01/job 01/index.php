9159fc95359ae48bc5fe0eee5131c99afee11f03
Lucas259746 <lucas.rozotte@laplateforme.io>
2025-08-01 10:43:01 +0200

    jour01 done

---
+++ jour01/job 01/index.php
@@ -0,0 +1,31 @@
+<?php
+$str = "LaPlateforme";
+$str1 = "Vive";
+$str2 = "!";
+echo "$str1 $str $str2"; // Affiche "Vive laplateforme !"
+?><br>
+
+<?php
+$val = 6;
+echo "$val";// Affiche 6
+?><br>
+
+<?php
+$val2 = 4;
+echo "$val2";// Affiche 4
+?><br>
+
+<?php
+$resultat = $val + $val2;
+echo "$resultat"; // Affiche 10
+?><br>
+
+<?php
+$MyBool = true;
+echo $MyBool; // Affiche 1 (true)
+?><br>
+
+<?php
+$MyBool = false;
+echo $MyBool; // N'affiche rien (false)
+?><br>
