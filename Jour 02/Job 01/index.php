0a8ecd8469ce38d1f0a8b9b3a04b284b1a44d0e5
Lucas259746 <lucas.rozotte@laplateforme.io>
2025-08-04 09:26:42 +0200

    job 01 done

---
+++ Jour 02/Job 01/index.php
@@ -0,0 +1,9 @@
+<?php
+for ($valeur = 0; $valeur <= 1337; $valeur++) {
+    if ($valeur == 42) {
+        echo "<strong><u>$valeur</u></strong><br>";
+    } else {
+        echo "$valeur<br>";
+    }
+}
+?>
