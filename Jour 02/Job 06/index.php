4d21fb94f8c4171cdd1c696b98f21b325ed07cee
Lucas259746 <lucas.rozotte@laplateforme.io>
2025-08-04 14:32:37 +0200

    Jour 2 done

---
+++ Jour 02/Job 06/index.php
@@ -0,0 +1,11 @@
+<?php
+$largeur = 10;
+$hauteur = 5;
+
+for ($i = 0; $i < $hauteur; $i++) {
+    for ($j = 0; $j < $largeur; $j++) {
+        echo "*";
+    }
+    echo "<br>";
+}
+?>
---
+++ Jour 02/Job 07/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$hauteur = 10; // Modifiable
+
+echo "<pre>"; // Préserve les espaces et les retours à la ligne
+for ($i = 1; $i <= $hauteur; $i++) {
+    // Espaces avant
+    echo str_repeat(' ', $hauteur - $i);
+
+    if ($i == 1) {
+        echo "/\\";
+    } elseif ($i == $hauteur) {
+        echo "/" . str_repeat("_", ($i - 1) * 2) . "\\";
+    } else {
+        echo "/" . str_repeat(' ', ($i - 1) * 2) . "\\";
+    }
+
+    echo "<br>"; 
+}
+echo "</pre>";
+?>
