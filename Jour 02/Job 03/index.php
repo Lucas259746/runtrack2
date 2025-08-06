33515cf9b0534b5b4ed9bd80b282449ff7c7297a
Lucas259746 <lucas.rozotte@laplateforme.io>
2025-08-06 09:57:26 +0200

    Job 01 done

--- Jour 02/Job 03/index.php
+++ Jour 02/Job 03/index.php
@@ -2,7 +2,7 @@
 for ($valeur = 0; $valeur <= 100; $valeur++) {
     if ($valeur <= 20) {
         echo "<i>$valeur</i><br>";
-    } else if ($valeur == 42) {
+    } elseif ($valeur == 42) {
         echo "La Plateforme_<br>";
     } elseif ($valeur >= 25 && $valeur <= 50) {
         echo "<u>$valeur</u><br>";// && = entre 25 et 50
