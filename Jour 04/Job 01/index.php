d685e5c644c3f498696816f1ca6b991d3ee51efd
Lucas259746 <lucas.rozotte@laplateforme.io>
2025-08-06 09:57:57 +0200

    done

---
+++ Jour 04/Job 01/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Test $_GET</title>
+</head>
+<body>
+
+    <h2>Formulaire GET</h2>
+    <form method="get" action="">
+        <label for="nom">Nom :</label>
+        <input type="text" name="nom" id="nom"><br><br>
+
+        <label for="prenom">Prénom :</label>
+        <input type="text" name="prenom" id="prenom"><br><br>
+
+        <label for="email">Email :</label>
+        <input type="text" name="email" id="email"><br><br>
+
+        <input type="submit" value="Envoyer">
+    </form>
+
+    <?php
+    // Partie 2 : Affichage du nombre d'arguments GET
+    if (!empty($_GET)) {
+        $nb_arguments = count($_GET);
+        echo "<p>Nombre d'arguments passés en GET : <strong>$nb_arguments</strong></p>";
+    }
+    ?>
+
+</body>
+</html>
